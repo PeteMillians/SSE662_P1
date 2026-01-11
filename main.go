ca2a7311124f64c5662f60f7001ca5e87260f964
PeteMillians <pete@millians.com>
2026-01-11 11:59:59 -0500

    Add comments to main

--- main.go
+++ main.go
@@ -6,7 +6,13 @@ import (
 )
 
 func main() {
+	/*
+		Main executable to run the To-Do List Manager
+	*/
+
+	// Instantiate a TaskManager object
 	manager := &taskManager.TaskManager{}
 
+	// Run the Command Line utility
 	util.CommandLine(manager)
 }
